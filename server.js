897bd1a314b5c788a1014447fe7dd692e945f079
JT6100 <jt6100@gmail.com>
2022-04-06 12:31:53 -0700

    if appied addr and q variables have been added

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ http
   .createServer((request, response) => {
     response.writeHead(200, { "Content-Type": "text/plain" });
     response.end("Hello Node!\n");
+    let addr = request.url,
+      q = url.parse(addr, true);
+    filepath = "";
   })
   .listen(8080);
 
