70bf8d73ed4f193eb1abd0f0f5bc1a53fb172c99
JT6100 <jt6100@gmail.com>
2022-04-06 12:53:03 -0700

    if applied fs will have been fully implemented

--- server.js
+++ server.js
@@ -8,7 +8,22 @@ http
     response.end("Hello Node!\n");
     let addr = request.url,
       q = url.parse(addr, true);
+    filepath = "";
+
+    fs.appendFile(
+      "log.txt",
+      "URL: " + addr + "\nTimestamp: " + new Date() + "\n\n",
+      (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Added to log.");
+        }
+      }
+    );
     if (q.pathname.includes("documentation")) {
+      filepath = __dirname + "/documentation.html";
+    } else {
       filepath = "index.html";
     }
     fs.readFile(filepath, (err, datta) => {
