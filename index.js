c60cad576a191bc7b950b1df860c737905c02486
JT6100 <jt6100@gmail.com>
2022-04-13 12:01:12 -0700

    Update index.js

--- index.js
+++ index.js
@@ -10,4 +10,15 @@ app.get("/", (req, res) => {
   res.send("welcome to myFlix");
 });
 
+app.get("/test", function (req, res) {
+  response.send("test this is a test");
+});
+
 app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+app.listen(3000, function () {
+  console.log("Server is running on localhost3000");
+});
