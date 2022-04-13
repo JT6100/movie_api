a92a0f2bf0ebf4c21ffc413569b5281d837de6bb
JT6100 <jt6100@gmail.com>
2022-04-13 11:47:12 -0700

    app get updates

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const res = require("express/lib/response");
 const app = express();
-}:
 
 app.get('/movies', (req, res) => {
     res.status(200).json(topmovies);
 });
 
-app.get('/', (req, res) => {
-    res.send('welcome to myFlix');
-
+app.get("/", (req, res) => {
+  res.send("welcome to myFlix");
 });
+
+app.use(express.static("public"));
