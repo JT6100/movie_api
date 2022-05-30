4daa8e23f73adf05da85062cbc9a1a8ad7df62f9
JT6100 <97696638+JT6100@users.noreply.github.com>
2022-05-30 11:35:04 -0700

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get("/", (req, res) => {
 
 
 //gets list of data about moives
-app.get("/movies",
+app.get("/movies", passport.authenticate('jwt', { session: false}),
 (req, res) => {
   Movies.find()
   .then((movies) => {
